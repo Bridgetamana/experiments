31b184553e16d808be32849258caba629ce1a571
Bridget Amana <amanabridget03@gmail.com>
2026-05-30 17:19:21 +0100

    feat: add routing and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
 import ClickToView from './components/clicktoview/ClickToView';
 import SquashAndStretch from './components/squashandstretch/SquashAndStretch';
 
 function App() {
   return (
     <main className='app'>
-      <SquashAndStretch />
-      <ClickToView />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <section className='home'>
+              <h1>Small UI experiments built while I learn.</h1>
+              <p className='intro'>
+                Each page is a focused animation or interaction experiment. Pick
+                one to view.
+              </p>
+
+              <ul className='experiment-links'>
+                <li>
+                  <Link className='experiment-link' to='/squash-and-stretch'>
+                    Squash and Stretch
+                  </Link>
+                </li>
+                <li>
+                  <Link className='experiment-link' to='/click-to-view'>
+                    Click to View
+                  </Link>
+                </li>
+              </ul>
+            </section>
+          }
+        />
+        <Route path='/squash-and-stretch' element={<SquashAndStretch />} />
+        <Route path='/click-to-view' element={<ClickToView />} />
+      </Routes>
     </main>
   );
 }
